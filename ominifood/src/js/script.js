0b6a020c66541ef63544482ee2948aadd645c2a0
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-02-27 20:33:03 -0300

    Scroll control of sticky bar

--- ominifood/src/js/script.js
+++ ominifood/src/js/script.js
@@ -1,1 +1,17 @@
+let featureSection = document.getElementsByClassName('js--section-features')[0];
+let stickyBar = document.getElementsByClassName('js--sticky')[0];
+let lastScrollHeight = 0;
 
+// Verify window scroll and controls if it needs to show sticky bar or not.
+window.onscroll = function () {
+    const currentScrollHeight = window.scrollY;
+    const scrollDown = currentScrollHeight > lastScrollHeight;
+    lastScrollHeight = currentScrollHeight;
+
+    if (window.scrollY > (featureSection.scrollHeight + 55)) {
+        stickyBar.classList.add('sticky');
+    }
+    else if (!scrollDown) {
+        stickyBar.classList.remove('sticky');
+    }
+}
